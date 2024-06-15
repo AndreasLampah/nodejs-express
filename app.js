568c28361691214e7214709fc67703b4183f6b3e
AndreasLampah <andreaslampah97@gmail.com>
2024-06-16 00:30:43 +0800

    kirim data ke ejs/html

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const app       = express()
 const port      = 3000
 
 
+app.set('view engine', 'ejs')   //setting penggunaan template engine untuk express
+app.set('views', './view-ejs')  //setting penggunaan folder untuk menyimpan file .ejs
+
+
 app.get('/', (req,res)=>{
-    res.send('<h2>Hello World</h2><hr>')
+    res.render('index')
 })
 
 
 app.get('/profil', (req,res)=>{
-    res.send('<h1>Profil saya</h1>')
+    res.render('profil')
+})
+
+
+app.get('/pengalaman', (req,res)=>{
+    res.render('daftar-pengalaman', {
+        nama: 'Aji Kowiyu Uzumaki',
+        jenis_kelamin: 'L',
+        posisi: 'Sr. Programmer',
+        perusahaan: 'Agung Podomoro',
+        gaji: 9557000,
+        pajak: (this.gaji > 10000000) ? 'gaji anda kena pajak' : 'aman, gak perlu bayar pajak',
+    })
 })
 
 
